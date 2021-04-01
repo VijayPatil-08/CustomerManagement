29636ff7ff081a1440a24387fed500d8aac0b27f
VijayPatil-08 <vijuhy6428@gmail.com>
2021-04-01 12:20:01 +0530

    Commit Customer

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,71 @@
+﻿using CustomerManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CustomerManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        IRepo<Customer> _repo;
+        public CustomerController(IRepo<Customer> repo)
+        {
+            _repo = repo;
+        }
+
+        // GET: api/<CustomerController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> Get()
+        {
+            try
+            {
+                var customers = _repo.GetItems();
+                return Ok(customers);
+            }
+            catch(Exception e)
+            {
+                return BadRequest("error" + e.Message);
+            }
+        }
+
+        // GET api/<CustomerController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CustomerController>
+        [HttpPost]
+        public ActionResult<Customer> Post([FromBody] Customer customer)
+        {
+            try
+            {
+                _repo.AddItem(customer);
+                return Ok(customer);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("error" + e.Message);
+            }
+        }
+
+        // PUT api/<CustomerController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CustomerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/CustomerRepo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerManagement.Models
+{
+    public class CustomerRepo : IRepo<Customer>
+    {
+        public List<Customer> customers { get; set; }
+
+        public CustomerRepo()
+        {
+            customers = new List<Customer>()
+            {
+                new Customer
+                {
+                    Id=101,
+                    Name = "Alex",
+                    Age = 22
+
+                }
+            };
+        }
+
+        public void AddItem(Customer item)
+        {
+            //Logic her we can check id pereset
+            customers.Add(item);
+        }
+
+        public IEnumerable<Customer> GetItems()
+        {
+            return customers;
+        }
+    }
+}
+
